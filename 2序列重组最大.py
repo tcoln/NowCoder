efbae1b4a666518f03988e3700f6c4232db6d6dc
Tom <2468775007@qq.com>
2019-08-16 00:37:37 +0800

    360笔试题

--- 2序列重组最大.py
+++ 2序列重组最大.py
@@ -1,9 +1,11 @@
 import sys
+import collections as coll
 line = sys.stdin.readline().strip().split(' ')
 N, M = int(line[0]), int(line[1])
 nums1 = [int(num) for num in sys.stdin.readline().strip().split(' ')]
 nums2 = [int(num) for num in sys.stdin.readline().strip().split(' ')]
 nums2 = set(nums2)
+nums2 = coll.Counter(nums2)
 print nums1, nums2
 ans = []
 for m in range(M-1, -1, -1):
